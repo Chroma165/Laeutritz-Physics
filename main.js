dca01ef2810cd1491374bf1894dd5fdbe86c364a
Chroma165 <davidleutert@icloud.com>
2024-11-27 13:09:13 +0100

    I cooked fr this time

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const ctx = screen.getContext("2d");
 const testPoly = [
   { x: -100, y: -100 },
   { x: -100, y:  100 },
-  { x:  100, y: -100 },
   { x:  100, y:  100 },
+  { x:  100, y: -100 },
 ];
 
 const test = new Instance([500, 500], testPoly, 3, [2, 2], "#8800ff");
