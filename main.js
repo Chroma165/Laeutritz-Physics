15f6b1c51f1557e3fb02adc10526901e80dbe4ae
Chroma165 <davidleutert@icloud.com>
2024-11-27 11:59:30 +0100

    Removed unneccerary code (ignore spelling)

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const screen = document.querySelector("#screen");
-const heading = document.querySelector("#heading");
 const ctx = screen.getContext("2d");
 
 const testPoly = [
@@ -12,5 +11,3 @@ const testPoly = [
 const test = new Instance([500, 500], testPoly, 3, [2, 2], "#8800ff");
 
 test.draw(ctx);
-
-heading.innerHTML = "yeet";
