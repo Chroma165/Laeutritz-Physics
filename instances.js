fdfcf27bf595a9ead1a13229c04d9d88920b391c
Chroma165 <davidleutert@icloud.com>
2024-11-27 11:28:58 +0100

    Create instances.js

---
+++ instances.js
@@ -0,0 +1,28 @@
+class Instance {
+  constructor(pos, poly, mass, velo, color, ctx) {
+    this.pos = pos;
+    this.polyRel = poly;
+    this.polyAbs = poly;
+    this.massKg = mass;
+    this.veloVec = velo;
+    this.color = color;
+    this.ctx = ctx;
+  }
+  
+  calcPolyAbs() {
+    let polyAbs = this.poly;
+    polyAbs.forEach((element) => {
+      element.x += this.pos[0];
+      element.y += this.pos[1];
+    })
+  }
+  
+  draw(ctx) {
+    drawPoly(this.polyAbs, this.color, ctx);
+  }
+  
+  update(ctx) {
+    calcPolyAbs();
+    draw(ctx);
+  }
+}
